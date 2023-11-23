42cd4320e6b9f0b736ab4e087f388b0c24c6438c
Dimaspadma <dimaspadma@gmail.com>
2023-11-24 00:51:23 +0700

    static bundling

---
+++ main.js
@@ -0,0 +1,52 @@
+const generatePdf = () => {
+  var pengirim = $("input[name=pengirim]").val();
+  var nomorPengirim = $("input[name=nomorPengirim]").val();
+
+  var penerima1 = $("input[name=penerima1]").val();
+  var penerima2 = $("input[name=penerima2]").val();
+  var penerima3 = $("input[name=penerima3]").val();
+
+  var nomor1 = $("input[name=nomor1]").val();
+  var nomor2 = $("input[name=nomor2]").val();
+  var nomor3 = $("input[name=nomor3]").val();
+
+  var alamat1 = $("textarea[name=alamat1]").val();
+  var alamat2 = $("textarea[name=alamat2]").val();
+  var alamat3 = $("textarea[name=alamat3]").val();
+
+  var list = []
+
+  if (penerima1 !== "") {
+    list.push({
+      penerima: penerima1,
+      nomor: nomor1,
+      alamat: alamat1,
+      pengirim,
+      nomorPengirim,
+    })
+  }
+
+  if (penerima2 !== "") {
+    list.push({
+      penerima: penerima2,
+      nomor: nomor2,
+      alamat: alamat2,
+      pengirim,
+      nomorPengirim,
+    })
+  }
+
+  if (penerima3 !== "") {
+    list.push({
+      penerima: penerima3,
+      nomor: nomor3,
+      alamat: alamat3,
+      pengirim,
+      nomorPengirim,
+    })
+  }
+
+  pdfkit.generate(list)
+}
+
+$("#generate").on("click", generatePdf);
